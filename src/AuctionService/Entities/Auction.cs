b988b71af1a1d32204c720ca38072c17cfbdd140
Tanmaydevilhunter <tanmatnfsgta@gmail.com>
2025-01-21 15:47:50 -0500

    Auction Service Complete

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,16 @@
+namespace AuctionService.Entities;//put a .Foldername after the namespace class name
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice {get; set;} = 0;
+    public string Seller {get; set;}
+    public string Winner {get; set;}
+    public int? SoldAmount {get; set;} // Optional Property
+    public int? CurrentHighBid {get; set;}
+    public DateTime CreatedAt {get; set;} = DateTime.UtcNow;// Use UTC values for Postgress DataBase
+    public DateTime UpdatedAt {get; set;} = DateTime.UtcNow;
+    public DateTime AuctionEnd {get; set;}
+    public Status Status {get; set;}//Enum Data type defined in Entities Folder
+    public Item Item {get; set;}//Class Data type defined in Entitiy Folder it is One-One Mapped with the Item Model/Entitiy but in reverse option used for FK concept
+}
---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities;
+
+[Table("Items")]
+public class Item
+{
+    public Guid Id {get; set;}
+    public string Make {get; set;}
+    public string Model {get; set;}
+    public int Year {get; set;}
+    public string Color {get; set;}
+    public int Mileage {get; set;}
+    public string ImageUrl {get; set;}
+
+    // nav properties
+    public Auction Auction {get; set;}//Class Data type defined in Entitiy Folder it is One-One Mapped with the Item Model/Entitiy but in reverse option used for FK concept
+    public Guid AuctionId {get; set;}
+}
